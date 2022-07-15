d2ed7fb97ae1a884b5f8c47e6076055d7b9768b6
bigzhu <vermiliondun@gmail.com>
2022-07-15 10:34:56 +0800

    add jsonlint

--- config.lua
+++ config.lua
@@ -159,24 +159,26 @@ end
 -- how to use null-ls: https://alpha2phi.medium.com/neovim-for-beginners-lsp-using-null-ls-nvim-bd954bf86b40
 -- set a formatter, this will override the language server formatting capabilities (if it exists)
 
-local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup {
-  {
-    -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
-    command = "prettier",
-    ---@usage arguments to pass to the formatter
-    -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
-    extra_args = { "--print-with", "100" },
-    ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
-    filetypes = { "typescript", "typescriptreact", "markdown", "vimwiki", "javascript", "typescript", "typescriptreact",
-      "vue", "css", "scss", "less", "html", "json", "yaml" },
-  },
-}
+--local formatters = require "lvim.lsp.null-ls.formatters"
+--formatters.setup {
+--  {
+--    -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+--    command = "prettier",
+--    ---@usage arguments to pass to the formatter
+--    -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+--    extra_args = { "--print-with", "100" },
+--    ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+--    filetypes = { "typescript", "typescriptreact", "markdown", "vimwiki", "javascript", "typescript", "typescriptreact",
+--      "vue", "css", "scss", "less", "html", "yaml" },
+--  },
+--}
 
 -- set additional linters
 local linters = require "lvim.lsp.null-ls.linters"
+
 linters.setup {
   { command = "eslint", filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" } },
+  { command = "jsonlint", filetypes = { "json" } },
   --{ command = "flake8", filetypes = { "python" } },
   --{
   --  -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
