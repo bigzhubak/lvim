7728666199e087741afbc749709983e06053608b
bigzhu <vermiliondun@gmail.com>
2023-02-03 13:36:55 +0800

    add bash zsh format

--- formatters.lua
+++ formatters.lua
@@ -4,20 +4,20 @@
 -- space n  show null info
 local formatters = require("lvim.lsp.null-ls.formatters")
 formatters.setup({
-	{ filetypes = { "kotlin" }, command = "ktlint" }, -- brew install ktlint
-	{ filetypes = { "go" }, command = "gofmt" },
-	{
-		filetypes = {
-			"javascriptreact",
-			"javascript",
-			"typescriptreact",
-			"typescript",
-			"vue",
-		},
-		command = "prettier",
-	},
-	{ filetypes = { "markdown", "vimwiki", "graphql" }, command = "prettier" },
-	{ filetypes = { "sh" }, command = "shfmt" }, -- brew install shfmt
-	{ filetypes = { "lua" }, command = "stylua" },
-	{ filetypes = { "python" }, command = "autopep8" }, -- brew install autopep8
+  { filetypes = { "kotlin" }, command = "ktlint" }, -- brew install ktlint
+  { filetypes = { "go" }, command = "gofmt" },
+  {
+    filetypes = {
+      "javascriptreact",
+      "javascript",
+      "typescriptreact",
+      "typescript",
+      "vue",
+    },
+    command = "prettier",
+  },
+  { filetypes = { "markdown", "vimwiki", "graphql" }, command = "prettier" },
+  { filetypes = { "sh", "zsh" }, command = "shfmt" }, -- brew install shfmt
+  { filetypes = { "lua" }, command = "stylua" },
+  { filetypes = { "python" }, command = "autopep8" }, -- brew install autopep8
 })
