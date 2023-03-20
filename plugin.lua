d26b54ac17229a2ab05d5a19606ddcaf9cf2c321
bigzhu <vermiliondun@gmail.com>
2023-03-20 08:44:21 +0800

    辅助

--- plugin.lua
+++ plugin.lua
@@ -3,6 +3,7 @@
 -- :Lazy update
 local home = os.getenv("HOME")
 lvim.plugins = {
+	{ "Exafunction/codeium.vim" }, -- codeium
 	{ -- 让 markdown 中的代码块也能用上 lsp
 		"epwalsh/obsidian.nvim",
 		config = function()
